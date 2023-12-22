7f574878fa4d0962b0d54df670d9693ef242717b
BarkinSungu <barkinsungu@gmail.com>
2023-12-22 11:55:43 +0300

    Project added.
    Project folders added.
    Readme file added.
    Working screen recording added.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # AppiumAutomationPOCWithCsharp
+
+This project is an example of Appium automation using C#.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- **Appium Server**: Ensure that Appium server is installed and running on your machine. You can download Appium from [Appium official website](http://appium.io/).
+
+- **Android Emulator or Device**: Make sure you have an Android emulator or device configured and accessible.
+
+- **Visual Studio or Visual Studio Code**: You need a C# development environment to run and modify the code. You can download Visual Studio or Visual Studio Code from [Visual Studio](https://visualstudio.microsoft.com/) or [Visual Studio Code](https://code.visualstudio.com/).
+
+- **NuGet Packages**: Ensure that the necessary NuGet packages are installed. The required packages are managed automatically by Visual Studio or can be installed manually.
+
+## Running the Project
+
+Follow these steps to run the Appium automation project:
+
+1. Clone the project to your local machine.
+
+2. Open the project in Visual Studio or Visual Studio Code.
+
+3. Build the project to restore the NuGet packages.
+
+4. Open the AppiumAutomation.cs file.
+
+5. Review the InitializeAppium and SetCapabilities methods in the code. Adjust the capabilities based on your specific Appium setup and the target Android application.
+
+6. Make sure Appium server is running on 127.0.0.1:4723. 
+
+7. Run the project.
+
+8. Monitor the console output to ensure that the "Start chat" button is found and clicked.
---
+++ TestAutomationAgentPOC/AgentPOC.cs
@@ -0,0 +1,97 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using System;
+
+/// <summary>
+/// Class responsible for handling Appium automation tasks.
+/// </summary>
+class AppiumAutomation
+{
+    private AndroidDriver<AndroidElement> driver;
+
+    public AppiumAutomation(AndroidDriver<AndroidElement> driver)
+    {
+        this.driver = driver;
+    }
+
+    /// <summary>
+    /// Set up Appium capabilities and connect to the Appium server.
+    /// </summary>
+    public void InitializeAppium()
+    {
+        AppiumOptions appiumOptions = new AppiumOptions();
+
+        // Set capabilities (device, app details, etc.)
+        SetCapabilities(appiumOptions);
+
+        // Connect to Appium server
+        this.driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
+    }
+
+    /// <summary>
+    /// Set Appium capabilities.
+    /// </summary>
+    private void SetCapabilities(AppiumOptions appiumOptions)
+    {
+        // Device properties
+        appiumOptions.AddAdditionalCapability("appium:deviceName", "Pixel 6 API 31");
+        appiumOptions.AddAdditionalCapability("platformName", "Android");
+        appiumOptions.AddAdditionalCapability("appium:udid", "emulator-5554");
+        appiumOptions.AddAdditionalCapability("appium:platformVersion", "12.0");
+
+        // Application informations
+        appiumOptions.AddAdditionalCapability("appium:appPackage", "com.google.android.apps.messaging");
+        appiumOptions.AddAdditionalCapability("appium:appActivity", "com.google.android.apps.messaging.ui.ConversationListActivity");
+
+        // Other settings
+        appiumOptions.AddAdditionalCapability("appium:skipUnlock", true);
+        appiumOptions.AddAdditionalCapability("appium:noReset", false);
+        appiumOptions.AddAdditionalCapability("appium:automationName", "uiautomator2");
+    }
+
+
+    /// <summary>
+    /// Find and click the "Start chat" button.
+    /// </summary>
+    public void ClickStartChatButton()
+    {
+        string xpathExpression = "//android.widget.Button[@content-desc='Start chat']";
+        IWebElement startChatButton = this.driver.FindElement(By.XPath(xpathExpression));
+        startChatButton.Click();
+    }
+
+    /// <summary>
+    /// Close the application session.
+    /// </summary>
+    public void CloseAppiumSession()
+    {
+        this.driver.Quit();
+    }
+}
+
+/// <summary>
+/// Main program entry point.
+/// </summary>
+class Program
+{
+    static void Main()
+    {
+        // Create an instance of AppiumAutomation
+        AppiumAutomation appiumAutomation = new AppiumAutomation(null); // You can pass a valid AndroidDriver instance here
+
+        // Initialize Appium
+        appiumAutomation.InitializeAppium();
+
+        // Click the "Start chat" button
+        appiumAutomation.ClickStartChatButton();
+
+        // Print output indicating that the "Start chat" button was found and clicked
+        Console.WriteLine("Start chat button found and clicked.");
+
+        // Close the application session
+        appiumAutomation.CloseAppiumSession();
+    }
+}
